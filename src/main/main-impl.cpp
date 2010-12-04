de8a0ad7e423cb096a360269dbd39a5031bf075f
Jonathan Marchand <skulrik@gmail.com>
2010-12-03 21:50:12 -0500

    Add GPL v3 licencing.

--- src/main/main-impl.cpp
+++ src/main/main-impl.cpp
@@ -1,3 +1,23 @@
+/*
+===============================================================================
+    COPYING PERMISSION STATEMENT
+===============================================================================
+    This file is part of CPP_APP_TEMPLATE.
+
+    CPP_APP_TEMPLATE is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CPP_APP_TEMPLATE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CPP_APP_TEMPLATE.  If not, see <http://www.gnu.org/licenses/>.
+===============================================================================
+*/
 #include "main-impl.h"
 #include "ioc.h"
 #include "internationalization.h"
